2f595b140db6aa30c305da4380bd2778a9b1601d
Julia West <juliamae@gmail.com>
2011-10-28 15:33:55 -0400

    Adding a map showing station location to station page.

---
+++ app/assets/javascripts/stops.js
@@ -0,0 +1,16 @@
+$(function(){
+  
+  if ( $("#map").size() > 0 ) {
+    var container      = $("#map"),
+        stationLatLng = new L.LatLng( container.data("lat"), container.data("lon") ),
+        map           = new L.Map('map');
+    
+    map.setView( stationLatLng, 13).addLayer(
+      new L.TileLayer("http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
+        maxZoom: 18, 
+        attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
+      })
+    ).addLayer( new L.Marker(stationLatLng));
+  }
+  
+});
