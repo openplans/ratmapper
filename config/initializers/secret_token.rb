aa2fcd91f418faabb8244c7d6a3ae63f1742f348
Julia West <juliamae@gmail.com>
2011-10-28 12:15:08 -0400

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stationgrade::Application.config.secret_token = 'e210f2a9e3b553da15583fd4e521b1ab303abfcfe824a14a1f2758631904d04e92e02bd071e45dffc28a315c9476339aa0ba021e95c30d154e99b3a78c3b28d0'
