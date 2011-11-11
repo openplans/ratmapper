87d525701cf27e6c04417811f2d3b9e8c6e662d4
Julia West <juliamae@gmail.com>
2011-11-11 11:03:11 -0500

    Detecting location for center; Starting map way zoomed in.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,29 +1,47 @@
 $(function(){
   
   $("body#home.index").find("#map").each(function() {
-    var container     = $(this),
-        center        = new L.LatLng( container.data("lat"), container.data("lon") ),
-        map           = new L.Map('map');
     
-    map.setView( center, 11).addLayer(
-      new L.TileLayer("http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
-        maxZoom: 18, 
-        attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
-      })
-    );
+    var positionSuccess = function (position) {
+      var here = new L.LatLng(position.coords.latitude, position.coords.longitude);
+      initMap(here);
+    };
+
+    var positionError = function (message) {
+      var center        = new L.LatLng( "40.717304", "-73.956872" )
+      initMap(center);
+    };
     
-    $.getJSON( location.href, function(data){
-      var geojson = new L.GeoJSON();
-      
-      geojson.on('featureparse', function(e) {
-        if (e.properties && e.properties.popupContent){
-          e.layer.bindPopup(e.properties.popupContent);
-        }
+    var initMap = function( center ) {
+      var container = $(this),
+          map       = new L.Map('map');
+
+      map.setView( center, 17).addLayer(
+        new L.TileLayer("http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
+          maxZoom: 18, 
+          attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">'
+        })
+      );
+
+      $.getJSON( location.href, function(data){
+        var geojson = new L.GeoJSON();
+
+        geojson.on('featureparse', function(e) {
+          if (e.properties && e.properties.popupContent){
+            e.layer.bindPopup(e.properties.popupContent);
+          }
+        });
+
+        geojson.addGeoJSON(data);
+        map.addLayer(geojson);
+
       });
-      
-      geojson.addGeoJSON(data);
-      map.addLayer(geojson);
-      
-    });
+    };
+    
+    navigator.geolocation ? 
+      navigator.geolocation.getCurrentPosition(positionSuccess, positionError) : 
+      positionError('not supported');
+    
+    
   });
 });
