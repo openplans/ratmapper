c348cae385401d848867bac822b56b2c6790cc89
Julia West <juliamae@gmail.com>
2011-11-11 12:57:23 -0500

    Incident submission via map popup on home screen.

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -11,15 +11,23 @@ class IncidentsController < ApplicationController
   end
   
   def create
-    @incident = Incident.new params[:incident].merge(:stop_id => @stop.id)
-    
-    if @incident.save
+    if params[:incident][:kind].present? && params[:incident][:kind].length > 0
+      params[:incident][:kind].each do |kind|
+        Incident.create params[:incident].merge(:stop_id => @stop.id, :kind => kind)
+      end
       respond_to do |format|
-        format.html { redirect_to stop_incident_path(@stop, @incident) }
+        format.html { redirect_to stop_path(@stop) }
+        format.json { 
+          render :json => { 
+            :view => render_to_string(:partial => "stops/popup.html", :locals => { :stop => @stop, :incidents => @stop.incidents }) 
+          }
+        } 
       end
     else
+      @incident = @stop.incidents.build params[:incident]
+      
       respond_to do |format|
-        flash[:alert] = "Something's missing from your incident report." 
+        flash[:alert] = "Hey, was it a rat or garbage?" 
         format.html { render :action => :new }
       end
     end
